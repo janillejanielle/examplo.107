fa75e7293a7fe5f6ce96e04ef16ba83a206b8890
janillejanielle <janille.janielle2@gmail.com>
2022-09-07 11:41:55 -0300

    Novo commit

---
+++ src/test/java/com/project/aula107/examplo7/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.aula107.examplo7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
